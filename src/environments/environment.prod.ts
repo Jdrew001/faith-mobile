48984db4da96ab222f39ba793c0a5fa9253bbc4e
Drew Atkison <dtatkison@gmail.com>
2020-05-04 14:38:08 -0500

    Updated env prod file

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,5 +3,6 @@ export const environment = {
   PAY_PAL_URL: 'https://api.paypal.com/',
   ASSET_URL: '/assets/',
   BASE_URL: 'http://192.168.1.212:1337/',
-  JSON_URL: 'http://localhost:3000'
+  JSON_URL: 'http://localhost:3000',
+  IMG_URL: 'http://localhost:1337'
 };
