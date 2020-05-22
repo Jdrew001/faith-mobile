6656b7d86d0d300f15b454ee4423804ca9904132
Drew Atkison <dtatkison@gmail.com>
2020-05-22 17:45:32 -0500

    temporary fix for the connect page for testing

--- src/app/connect/connect.constant.ts
+++ src/app/connect/connect.constant.ts
@@ -1,5 +1,5 @@
 export class ConnectConstant {
-    public static FB_FEED = 'https://graph.facebook.com/v6.0/107643457281903?fields=posts%7Battachments%7Bdescription%2Cmedia%2Cmedia_type%2Ctitle%7D%2Cmessage%2Ccreated_time%7D&access_token=EAADGQ0IHbkYBAGo8jcxWPZAYrkZBxnE7ZAXW2eQR5C4mzU5ZBoasu27GFmUT5lMaSI5nmqXwk3SZCqhMpHkuCAt93QzaENjLQZAxJGF2T1pygu9vJBQRxdZBU9mchsMkxhCT9tmvyd7ympG80nFa9V08nDh4qldZBZADqnfIVTb0Qb1ZBTxtCZACZB2s5CP0ZAYTiZC3L6nVZCQxbZCCpAZDZD';
+    public static FB_FEED = 'https://graph.facebook.com/v6.0/107643457281903?fields=posts%7Battachments%7Bdescription%2Cmedia%2Cmedia_type%2Ctitle%7D%2Cmessage%2Ccreated_time%7D&access_token=EAADGQ0IHbkYBABiSvKsZAs3QEb3FGZAVnYB2ZBrUnRbzG9yGsxw5ZC5xX3JS0zV8alZCBv3LyICryIDSovaAaQXtIM29p4cWXGZAvCV0SeaZC4ZAnLsAP65FFEEpNlNFmvdeZA0vgUtuUcqsO4jQLgIRTDuwlffkb8HwGRk0XCd7xDianCCtOrSOw86BWZCfWxXfgZD';
     public static BIBLE_STUDY_URL = 'biblestudies';
     public static DEVICE_PLATFORM = {
         Android: 'android',
