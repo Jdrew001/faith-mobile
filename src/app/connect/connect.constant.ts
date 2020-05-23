071f6cdda464d652bd5f31d301c9b14ec39076b2
Drew <dtatkison@gmail.com>
2020-05-23 11:34:36 -0500

    token update for facebook api

--- src/app/connect/connect.constant.ts
+++ src/app/connect/connect.constant.ts
@@ -1,5 +1,5 @@
 export class ConnectConstant {
-    public static FB_FEED = 'https://graph.facebook.com/v6.0/107643457281903?fields=posts%7Battachments%7Bdescription%2Cmedia%2Cmedia_type%2Ctitle%7D%2Cmessage%2Ccreated_time%7D&access_token=EAADGQ0IHbkYBABiSvKsZAs3QEb3FGZAVnYB2ZBrUnRbzG9yGsxw5ZC5xX3JS0zV8alZCBv3LyICryIDSovaAaQXtIM29p4cWXGZAvCV0SeaZC4ZAnLsAP65FFEEpNlNFmvdeZA0vgUtuUcqsO4jQLgIRTDuwlffkb8HwGRk0XCd7xDianCCtOrSOw86BWZCfWxXfgZD';
+    public static FB_FEED = 'https://graph.facebook.com/v6.0/107643457281903?fields=posts%7Battachments%7Bdescription%2Cmedia%2Cmedia_type%2Ctitle%7D%2Cmessage%2Ccreated_time%7D&access_token=EAADGQ0IHbkYBAOxsBZBJU8GGEfx0mHdAdT4rO4OSuZCScJWInLEz3tL1z5IlslHLzQg5OLvTRfuVyGUWzZClUPBtLCopFzj0WexQOt3FTVezyjMOJdH8sei7P9ZA4pqhZCrUuXrqexuSHEz8OlcwJbIsBZCYthNZBGZANrN6S2ZAzaajLYgZAePEZBP';
     public static BIBLE_STUDY_URL = 'biblestudies';
     public static DEVICE_PLATFORM = {
         Android: 'android',
