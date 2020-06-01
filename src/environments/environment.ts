aac5340748a8bcd5b99c7dda3483ff2c61394a74
Drew Atkison <dtatkison@gmail.com>
2020-05-31 21:49:37 -0500

    updated font size

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,10 +6,11 @@ export const environment = {
   production: false,
   PAY_PAL_URL: 'https://api.paypal.com/',
   ASSET_URL: '/assets/',
-  //BASE_URL: 'http://localhost:1337/',
-  BASE_URL: 'http://atkisondevserver.me:1337/',
+  BASE_URL: 'http://localhost:1337/',
+  //BASE_URL: 'http://atkisondevserver.me:1337/',
   //BASE_URL: 'http://192.168.1.212:1337/',
-  IMG_URL: 'http://atkisondevserver.me:1337',
+  // IMG_URL: 'http://atkisondevserver.me:1337',
+  IMG_URL: 'http://localhost:1337',
   JSON_URL: 'http://localhost:3000'
 };
 
