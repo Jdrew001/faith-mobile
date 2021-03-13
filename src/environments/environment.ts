e1158a20c02d60fc36f91170bbca1b1010a6514b
Drew Atkison <dtatkison@gmail.com>
2021-03-13 17:59:37 -0600

    prod url updated

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,6 +6,6 @@ export const environment = {
   CMS_URL: 'http://localhost:1337',
   //BASE_URL: 'http://atkisondevserver.me:1448/',
   //BASE_URL: 'http://192.168.1.212:1337/',
-  IMG_URL: 'http://atkisondevserver.me:1337',
+  IMG_URL: 'http://localhost:1337',
   JSON_URL: 'http://localhost:3000'
 };
