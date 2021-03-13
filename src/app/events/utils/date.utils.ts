91526f3a5068bc3b83b99a098bd8aeb33d53873d
Drew Atkison <dtatkison@gmail.com>
2021-03-13 14:40:03 -0600

    date utilities added for weekly and monthly

---
+++ src/app/events/utils/date.utils.ts
@@ -0,0 +1,59 @@
+import { Injectable } from "@angular/core";
+import * as moment from 'moment';
+
+export enum Frequency {
+    MONTHLY = "MONTHLY",
+    WEEKLY = "WEEKLY"
+}
+
+@Injectable()
+export class DateUtils {
+    
+    // GET DATE RANGE 
+    getDateRange(frequency: Frequency, startDate: string, endDate: string) {
+        let result: Array<String>;
+        console.log('freq', frequency);
+        switch(frequency) {
+            case Frequency.MONTHLY:
+                return this.getMonthlyDates(startDate, endDate);
+            case Frequency.WEEKLY:
+                return this.getWeeklyDates(startDate, endDate);
+        }
+    }
+
+    private getMonthlyDates(start: string, end: string) {
+        let startDate = moment(start);
+        let endDate = moment(end);
+        let weekNum = Math.ceil(startDate.date() / 7);
+        let arr = [];
+
+        let temp = startDate.clone().day(start);
+        arr.push(temp.format('YYYY-MM-DD'));
+
+        while(temp.isBefore(endDate.add(1))) {
+            temp.add(7, 'days');
+
+            let tempWeekNum = Math.ceil(temp.date() / 7);
+            if (tempWeekNum == weekNum) {
+                arr.push(temp.format('YYYY-MM-DD'));
+            }
+        }
+
+        return arr;
+    }
+
+    private getWeeklyDates(start: string, end: string) {
+        let startDate = moment(start);
+        let endDate = moment(end);
+        let arr = [];
+        let temp = startDate.clone().day(start);
+        arr.push(temp.format('YYYY-MM-DD'));
+
+        while(temp.isBefore(endDate)) {
+            temp.add(7, 'days');
+            arr.push(temp.format('YYYY-MM-DD'));
+        }
+
+        return arr;
+    }
+}
