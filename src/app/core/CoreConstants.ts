a66a0768a7fe0427a9dc06d6ae10d2f9ef19b61b
Drew <dtatkison@gmail.com>
2021-03-20 10:09:01 -0500

    update to native app

--- src/app/core/CoreConstants.ts
+++ src/app/core/CoreConstants.ts
@@ -1,6 +1,6 @@
 export class CoreConstants {
     public static LOAD_TIME = 800;
-    public static PHONE_TOKEN_URL = '/mobiletokens';
+    public static PHONE_TOKEN_URL = 'mobiletokens';
     public static PRAYER_REQUEST = 'prayer-requests';
     public static TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVlYjAyZjQxODcxYWVmMGUyOGE2MGU2YyIsImlzQWRtaW4iOnRydWUsImlhdCI6MTU4OTMyNDA3MywiZXhwIjoxNTkxOTE2MDczfQ.iRdj5VDHrZ_fmmTsS3Vg0L6-m-Vtt5d6COlGWFQOGew';
     public static TOKEN_URL = 'auth/local';
